01b4423a1b22568cc1c2e74edb08a8354c48a38d
Luis <luispaulo.ni@gmail.com>
2024-10-13 22:19:40 -0300

    callback to cors

--- lib/config/corsOptions.ts
+++ lib/config/corsOptions.ts
@@ -2,6 +2,13 @@ import { CorsOptions } from 'cors'
 import 'dotenv/config'
 
 export const corsOptions: CorsOptions = {
-    origin: process.env.URL,
+    origin: (origin, callback) => {
+        if (origin === process.env.URL || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
 }
