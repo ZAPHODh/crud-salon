5687e2e88e5682a334db74fc5de62dd0d925fa55
Luis <luispaulo.ni@gmail.com>
2024-10-17 18:00:04 -0300

    updated expnese log

--- controllers/expenses/updateExpense.ts
+++ controllers/expenses/updateExpense.ts
@@ -5,9 +5,16 @@ export const updateExpense = async (req: Request, res: Response) => {
     const { id } = req.params
     try {
         const expense: Expense = req.body
-        await expenseModel.updateOne({ _id: id }, expense)
-        console.log(expense)
-        return res.status(200).json({ message: 'expense updated' })
+        const updatedExpense = await expenseModel.updateOne(
+            { _id: id },
+            expense,
+            { new: true }
+        )
+        console.log(updatedExpense)
+        if (!updatedExpense) {
+            return res.status(404).json({ error: 'Expense not found' })
+        }
+        return res.status(200).json()
     } catch {
         return res.status(400).json({ error: 'failed to update the expense' })
     }
